fa8cd704675e469c35a7f1132b6a26eadebfed1f
DebugDamien <119611108+Damienb123@users.noreply.github.com>
2024-06-06 13:01:19 -0500

    Update README.md
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Spelling lol
    
    Update README.md
    
    Update README.md
    
    started the project with daisyui components and tailwindcss
    
    updated README.md with front-end stack
    
    navbar and some of hero section done

---
+++ front-end/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import NavBar from './layouts/NavBar'
+import Footer from './layouts/Footer'
+import RootLayout from './layouts/RootLayout'
+function App() {
+  return (
+    <>
+      <NavBar />
+      <RootLayout />
+      <Footer />
+    </>
+  )
+}
+
+export default App
