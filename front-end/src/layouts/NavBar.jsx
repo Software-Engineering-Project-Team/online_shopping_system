f7b9d24c915f84ff90668e54dbb4975214f5666e
Brianna Anaya Carrasco <126296126+briannanaya@users.noreply.github.com>
2024-07-23 15:52:09 -0500

    Update NavBar.jsx
    Added to be able to click on cart and move to checkout

--- front-end/src/layouts/NavBar.jsx
+++ front-end/src/layouts/NavBar.jsx
@@ -1,6 +1,7 @@
-import { FaShoppingCart, FaSearch} from "react-icons/fa";
+import { FaShoppingCart, FaSearch } from "react-icons/fa";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { IconContext } from "react-icons";
+import { Link } from "react-router-dom"; 
 
 function Navbar() {
   return (
@@ -14,7 +15,7 @@ function Navbar() {
             Foodie
           </a>
         </div>
-        <div className=" m-4">
+        <div className="m-4">
           <input type="text" placeholder="Search Products..." className="input input-bordered w-full md:w-auto bg-[#30C976] rounded-none bg-opacity-10" />
           <a href="" className="btn btn-ghost bg-[#30C976] rounded-none bg-opacity-80">
             <IconContext.Provider value={{ color: "#fff" }}>
@@ -23,12 +24,14 @@ function Navbar() {
           </a>
         </div>
         <div className="flex justify-between gap-x-4 w-full md:w-auto">
-          <button tabIndex={0} className="btn btn-ghost rounded-none bg-[#30C976] text-xl text-white">
-          Cart
-            <IconContext.Provider value={{ color: "#fff" }}>
-              <MdOutlineShoppingCart size={24} />
-            </IconContext.Provider>
-          </button>
+          <Link to="/checkout-summary">
+            <button tabIndex={0} className="btn btn-ghost rounded-none bg-[#30C976] text-xl text-white">
+              Cart
+              <IconContext.Provider value={{ color: "#fff" }}>
+                <MdOutlineShoppingCart size={24} />
+              </IconContext.Provider>
+            </button>
+          </Link>
           <button tabIndex={0} className="btn btn-ghost rounded-none bg-[#fff] text-xl text-black">Register</button>
           <button tabIndex={0} className="btn btn-ghost rounded-none bg-[#30C976] text-xl text-white">Login</button>
         </div>
@@ -37,4 +40,4 @@ function Navbar() {
   )
 }
 
-export default Navbar
+export default Navbar;
