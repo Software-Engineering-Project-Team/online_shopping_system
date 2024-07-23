bc93f4ad59534540fad167655216c638373eb265
Brianna Anaya Carrasco <126296126+briannanaya@users.noreply.github.com>
2024-07-23 15:40:55 -0500

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './layouts/NavBar';
@@ -26,36 +26,45 @@ import FanFavoritesProducts from './components/FanFavoritesProducts';
 import SeasonalProducts from './components/SeasonalProducts';
 import BBQProducts from './components/BBQProducts';
 import HealthyLivingProducts from './components/HealthyLivingProducts';
+import CheckoutSummary from './components/CheckoutSummary';
 
 function App() {
+  const [cart, setCart] = useState([]);
+  
+  const addToCart = (item) => {
+    setCart([...cart, item]);
+  };
+
   return (
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<RootLayout />} />
-        <Route path="/all-products" element={<AllProducts />} />
-        <Route path="/new" element={<NewProducts />} />
-        <Route path="/on-sale" element={<OnSaleProducts />} />
-        <Route path="/dairy" element={<DairyProducts />} />
-        <Route path="/produce" element={<ProduceProducts />} />
-        <Route path="/meat" element={<MeatProducts />} />
-        <Route path="/deli" element={<DeliProducts />} />
-        <Route path="/frozen" element={<FrozenProducts />} />
-        <Route path="/bakery" element={<BakeryProducts />} />
-        <Route path="/snacks" element={<SnacksProducts />} />
-        <Route path="/pantry" element={<PantryProducts />} />
-        <Route path="/breakfast" element={<BreakfastProducts />} />
-        <Route path="/beverages" element={<BeveragesProducts />} />
-        <Route path="/alcohol" element={<AlcoholProducts />} />
-        <Route path="/baby" element={<BabyProducts />} />
-        <Route path="/pet" element={<PetProducts />} />
-        <Route path="/household" element={<HouseholdProducts />} />
-        <Route path="/personal-care" element={<PersonalCareProducts />} />
-        <Route path="/fan-favorites" element={<FanFavoritesProducts />} />
-        <Route path="/seasonal" element={<SeasonalProducts />} />
-        <Route path="/bbq" element={<BBQProducts />} />
-        <Route path="/healthy-living" element={<HealthyLivingProducts />} />
+      <Route path="/" element={<RootLayout />} />
+        <Route path="/all-products" element={<AllProducts addToCart={addToCart} />} />
+        <Route path="/new" element={<NewProducts addToCart={addToCart} />} />
+        <Route path="/on-sale" element={<OnSaleProducts addToCart={addToCart} />} />
+        <Route path="/dairy" element={<DairyProducts addToCart={addToCart} />} />
+        <Route path="/produce" element={<ProduceProducts addToCart={addToCart} />} />
+        <Route path="/meat" element={<MeatProducts addToCart={addToCart} />} />
+        <Route path="/deli" element={<DeliProducts addToCart={addToCart} />} />
+        <Route path="/frozen" element={<FrozenProducts addToCart={addToCart} />} />
+        <Route path="/bakery" element={<BakeryProducts addToCart={addToCart} />} />
+        <Route path="/snacks" element={<SnacksProducts addToCart={addToCart} />} />
+        <Route path="/pantry" element={<PantryProducts addToCart={addToCart} />} />
+        <Route path="/breakfast" element={<BreakfastProducts addToCart={addToCart} />} />
+        <Route path="/beverages" element={<BeveragesProducts addToCart={addToCart} />} />
+        <Route path="/alcohol" element={<AlcoholProducts addToCart={addToCart} />} />
+        <Route path="/baby" element={<BabyProducts addToCart={addToCart} />} />
+        <Route path="/pet" element={<PetProducts addToCart={addToCart} />} />
+        <Route path="/household" element={<HouseholdProducts addToCart={addToCart} />} />
+        <Route path="/personal-care" element={<PersonalCareProducts addToCart={addToCart} />} />
+        <Route path="/fan-favorites" element={<FanFavoritesProducts addToCart={addToCart} />} />
+        <Route path="/seasonal" element={<SeasonalProducts addToCart={addToCart} />} />
+        <Route path="/bbq" element={<BBQProducts addToCart={addToCart} />} />
+        <Route path="/healthy-living" element={<HealthyLivingProducts addToCart={addToCart} />} />
+        <Route path="/checkout-summary" element={<CheckoutSummary cart={cart} setCart={setCart} />} />
       </Routes>
+
       <Footer />
     </Router>
   );
